98db010d1f032f04a1c73fb29eaae6dc6faadd24
Enrichson <enrichsonparis@gmail.com>
2025-05-01 11:24:46 +0800

    Fixed logging in issue and home.html typo (#14)

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if current_user.is_authenticated:
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
   form = LoginForm()
   if form.validate_on_submit():
     user = db.session.scalar(
@@ -37,7 +37,7 @@ def login():
     login_user(user, remember=form.remember_me.data)
     next_page = request.args.get('next')
     if not next_page or urlsplit(next_page).netloc != '':
-      next_page = url_for('index')
+      next_page = url_for('home')
     return redirect(next_page)
   return render_template('login.html', title='Sign In', form=form)
 
