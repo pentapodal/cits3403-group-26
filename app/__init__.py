2b772ed26439eedef3578422fb8bf6d1f4099b9e
Enrichson Paris <enrichsonparis@gmail.com>
2025-05-16 00:01:24 +0800

    made create_application() in init and blueprint.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+def create_application(config):
+  app = Flask(__name__)
+  app.config.from_object(config)
+  db = SQLAlchemy(app)
+  migrate = Migrate(app, db)
+  login = LoginManager(app)
+  login.login_view = 'login'
+  return app
 
 from app import routes, models
